7eeca2c1c8db5c402f09c45f4c28d31c0b487071
mrcruz117 <mrcruz1989@gmail.com>
2022-01-06 16:20:29 -0500

    deleted unnecessary files and Navbar init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
